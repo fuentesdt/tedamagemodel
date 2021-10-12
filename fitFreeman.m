3093a6bc259972d403b1b738b8593b23762392d9
David Fuentes <fuentesdt@gmail.com>
2021-10-12 18:52:05 -0500

    WIP: curve fit

--- fitFreeman.m
+++ fitFreeman.m
@@ -19,3 +19,24 @@ semilogy(base15min.minute,base15min.survival)
 semilogy(acid15min.minute,acid15min.survival)
 semilogy(base20min.minute,base20min.survival)
 semilogy(acid20min.minute,acid20min.survival)
+
+
+% setup curve fit
+Ea0 = optimvar('Ea0','LowerBound',0);
+Ea1 = optimvar('Ea1','LowerBound',0);
+
+
+disp('build objective function')
+mycostfcn = log( base10min.survival.^(-1)) - ...
+   deltaTheat10min * frequencyfactor * exp(-(Ea0+Ea1* pHheat)/(GasConst * Theat10min)) - ...
+   base10min.minute * frequencyfactor * exp(-(Ea0+Ea1* pHincbase)/(GasConst * Tinc))
+
+
+disp('create optim prob')
+convprob = optimproblem('Objective',mycostfcn );
+%show(convprob)
+problem = prob2struct(convprob,'ObjectiveFunctionName','generatedObjective');
+%% 
+% Solve the new problem. The solution is essentially the same as before.
+myoptions = optimoptions(@lsqnonlin,'Display','iter-detailed');
+[popt,fval,exitflag,output] = solve(convprob,x0,'Options',myoptions, 'solver','lsqnonlin' )
