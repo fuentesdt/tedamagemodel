3e65b78afa3e3d56fec52409cc18cbb9a30bea43
David Fuentes <fuentesdt@gmail.com>
2022-04-21 10:26:37 -0500

    DOC: usage

--- fitSummary.m
+++ fitSummary.m
@@ -1,9 +1,12 @@
-% opts.format='pdf'; opts.outputDir='.'; publish('fitFreeman.m',opts);
+% opts.format='pdf'; opts.outputDir='.'; publish('fitSummary.m',opts);
+% opts.format='pdf'; opts.outputDir='hepg2'; publish('fitSummary.m',opts);
 clear all
 close all
 
 % load 2 data 
-summarydata = readtable('summarydata.csv');
+%summarydata = readtable('summarydata.csv');
+summarydata = readtable('combinedhepg2data.csv');
+
 summarydata.Temperature = summarydata.Temperature + 273;
 %summarydata.pH = summarydata.pH ;
 summarydata.Time = summarydata.Time * 60 ;
@@ -50,10 +53,13 @@ myoptions = optimoptions(@lsqnonlin,'Display','iter-detailed','OptimalityToleran
 x0.Ea0 = 6.28e5; % J/mol
 x0.Ea1 = 0;
 x0.logA = log(3.1e98);
+% solve for temperature  and pH 
 [popt,fval,exitflag,output] = solve(convprob,x0,'Options',myoptions, 'solver','lsqnonlin' )
+% solve for temperature only
 [popttemp,fvaltemp,exitflagtemp,outputtemp] = solve(tempprob,x0,'Options',myoptions, 'solver','lsqnonlin' )
 
 
+% solve for pH with popttemp.logA  popttemp.Ea0 from the temperature solve
 phcostfcn = sum((log( pHsubset.Viabilty.^(-1)) - ...
    pHsubset.Time.* exp(popttemp.logA -popttemp.Ea0*(GasConst * pHsubset.Temperature +Ea1* pHsubset.pH ).^(-1)) ).^2)  
 phprob   = optimproblem('Objective',  phcostfcn );
@@ -63,26 +69,28 @@ initobjfunc = evaluate(fullcostfcn ,x0)
 optobjefunc = evaluate(fullcostfcn ,popt)
 
 %evaluate fit
-damageprediction= exp(popt.logA ) * summarydata.Time.*exp(-popt.Ea0*(GasConst * summarydata.Temperature+popt.Ea1* summarydata.pH).^(-1));
+damageprediction    = exp(    popt.logA ) * summarydata.Time.*exp(-popt.Ea0    *(GasConst * summarydata.Temperature+popt.Ea1 *  summarydata.pH).^(-1));
 damagepredictionstep= exp(popttemp.logA ) * summarydata.Time.*exp(-popttemp.Ea0*(GasConst * summarydata.Temperature+poptph.Ea1* summarydata.pH).^(-1));
-measureddamage =  log( summarydata.Viabilty.^(-1));
-
-damagepredictionph= exp(popttemp.logA ) * pHsubset.Time.*exp(-popttemp.Ea0*(GasConst * pHsubset.Temperature+poptph.Ea1* pHsubset.pH).^(-1));
-measureddamageph =  log( pHsubset.Viabilty.^(-1));
+damagepredictionph  = exp(popttemp.logA ) *    pHsubset.Time.*exp(-popttemp.Ea0*(GasConst *    pHsubset.Temperature+poptph.Ea1*    pHsubset.pH).^(-1));
+measureddamage   =  log( summarydata.Viabilty.^(-1));
+measureddamageph =  log(    pHsubset.Viabilty.^(-1));
 
 damagepredictiontemp= exp(popttemp.logA ) * temperaturesubset.Time.*exp(-(popttemp.Ea0                                   )./(GasConst * temperaturesubset.Temperature));
 measureddamagetemp =  log( temperaturesubset.Viabilty.^(-1));
 
 % get Rsquared
+% insample compare solve with Ea0 Ea1 and A fit to all data
+% insample compare measured and predicted
 mdl = fitlm(measureddamage ,damageprediction)
 handlefit=figure(5)
 plot(mdl)
 xlabel( 'measured damage')
 ylabel( 'predicted damage')
 title(sprintf('R^2=%f, A=%9.2e, Ea0=%9.2e, Ea1=%9.2e',mdl.Rsquared.Ordinary,exp(popt.logA ) ,popt.Ea0,popt.Ea1))
-saveas(handlefit,'ArrheniusFit','png')
+saveas(handlefit,'ArrheniusFit1','png')
 
 % get Rsquared
+% solve temperature first then fit pH - predict on pH subset ONLY
 mdlph = fitlm(measureddamageph ,damagepredictionph)
 handlefit=figure(6)
 plot(mdlph)
@@ -92,6 +100,7 @@ title(sprintf('R^2=%f, A=%9.2e, Ea0=%9.2e, Ea1=%9.2e',mdlph.Rsquared.Ordinary,ex
 saveas(handlefit,'ArrheniusFitph','png')
 
 % get Rsquared
+% temperature only fit
 mdltemp = fitlm(measureddamagetemp ,damagepredictiontemp)
 handlefit=figure(7)
 plot(mdltemp)
@@ -101,6 +110,7 @@ title(sprintf('R^2=%f, A=%9.2e, Ea0=%9.2e',mdltemp.Rsquared.Ordinary,exp(popttem
 saveas(handlefit,'ArrheniusFittemp','png')
 
 % get Rsquared
+% solve temperature first then fit pH - insample prediction on full dataset
 mdlstep = fitlm(measureddamage ,damagepredictionstep)
 handlefit=figure(8)
 plot(mdlstep)
